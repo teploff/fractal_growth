3471d22e448f6874893f3c51ea498974beddb490
Alexander <teploff.a@mail.ru>
2020-03-29 03:14:55 +0300

    make two and three three level tree

--- main.py
+++ main.py
@@ -4,21 +4,16 @@ import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
 import design  # Это наш конвертированный файл дизайна
 from OpenGL.GL import *
-from anytree import RenderTree
 # from OpenGL.GLU import *
 from geometry.entity_2d import Point, Segment
-from ontogeny.entity_2d import Branch, PlantTree
-from ontogeny.utils import engender_random_buds, engender_branch, increase_segment
+from ontogeny.utils import increase_segment
+from ontogeny.examles import get_two_level_tree, get_three_level_tree
 import random
 
-from anytree import PreOrderIter
 
 MIN_ANGLE = -10
 MAX_ANGLE = 5
 LENGTH_K = 1.1
-
-# import random
-import time
 
 SCROLL_UP = 4
 SCROLL_DOWN = 5
@@ -39,50 +34,9 @@ class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
         display = (WIDTH, HEIGHT)
         pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
 
-        # self.draw(lines)
         quit_mode = False
-        p1 = Point(0, -1)
-        p2 = engender_branch(p1, 0.5, 90)
-        l1 = Segment(p1, p2)
-        print("Angle = ", l1.get_triangle_angle())
-        root = Branch(l1)
-        tree = PlantTree(root)
-        p3, p4 = engender_random_buds(l1, 2)
-        p5 = engender_branch(p3, 0.4, 45)
-        p6 = engender_branch(p4, 0.4, 135)
-        l2 = Segment(p3, p5)
-        print("Angle 2 = ", l2.get_triangle_angle())
-        l3 = Segment(p4, p6)
-        print("Angle 3 = ", l3.get_triangle_angle())
-        node1 = Branch(l2, parent=root)
-        node2 = Branch(l3, parent=root)
-        # p7, p8 = engender_random_buds(Segment(p3, p5), 2)
-        # p9 = engender_branch(p7, 0.3, 67.5)
-        # p10 = engender_branch(p8, 0.2, 22.5)
-        # l4 = Segment(p7, p9)
-        # l5 = Segment(p8, p10)
-        # node3 = TreeBranch(l4, parent=node1)
-        # node4 = TreeBranch(l5, parent=node1)
-        # p11, p12 = engender_random_buds(Segment(p4, p6), 2)
-        # p13 = engender_branch(p11, 0.4, 157.5)
-        # p14 = engender_branch(p12, 0.4, 112.5)
-        # l6 = Segment(p12, p14)
-        # l7 = Segment(p11, p13)
-        # node5 = TreeBranch(l6, parent=node2)
-        # node6 = TreeBranch(l7, parent=node2)
-        # for pre, fill, node in RenderTree(root):
-        #     print("%s%s" % (pre, node))
-        # print("1 = ", node1.get_branch(1))
-        # print("2 = ", node1.get_branch(2))
-        # print("3 = ", node1.get_branch(2))
-
-        # p5 = create_point(p4, 0.1, 180)
-        # p6 = create_point(p5, 0.1, 225)
-        # p7 = create_point(p6, 0.1, 270)
-        # p8 = create_point(p7, 0.1, 315)
-        # p9 = create_point(p8, 0.1, 360)
-        # p10 = create_point(p9, 0.1, 45)
-        segments = tree.get_branches_as_segments(root)
+        tree = get_three_level_tree()
+        segments = tree.get_branches_as_segments(tree.root)
         while not quit_mode:
             selected_index_branch = random.randint(0, len(tree) - 1)
             self.draw(segments, selected_index_branch)
@@ -98,7 +52,7 @@ class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 child.segment.finish.x += delta_x
                 child.segment.finish.y += delta_y
             tree.update_branch(segments[selected_index_branch], new_branch)
-            segments = tree.get_branches_as_segments(root)
+            segments = tree.get_branches_as_segments(tree.root)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
